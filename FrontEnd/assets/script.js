d0f6c15b2731a79809fb5fbd014c8edff4f866e5
Styrale <styrale.draw@gmail.com>
2023-05-15 11:32:18 +0200

    page d'acceuil v1

---
+++ FrontEnd/assets/script.js
@@ -0,0 +1,159 @@
+fetch('http://localhost:5678/api/works')
+.then(response => response.json())
+.then(data => {
+  // console.log(data);
+
+  // const imageURLs = data.map(item => item["imageUrl"]);
+  // console.log(imageURLs);
+    afficherImages(data);
+  })
+.catch(error => {
+  console.error('Une erreur s\'est produite :', error);
+});
+
+function afficherImages(maListeObjetImage) {
+  const gallery = document.getElementById('gallery');
+
+  maListeObjetImage.map(unSeulObjetImage => {
+    const img = document.createElement('img');
+    img.src = unSeulObjetImage["imageUrl"];
+    img.title = unSeulObjetImage["title"];
+    console.log(gallery);
+
+    const title = document.createElement('p');
+    title.textContent = unSeulObjetImage.title;
+    
+    const container = document.createElement('div');
+    container.appendChild(img);
+    container.appendChild(title);
+
+    gallery.appendChild(container);
+  });
+}
+
+const works = [
+  {
+    "id": 1,
+    "title": "Abajour Tahina",
+    "imageUrl": "http://localhost:5678/images/abajour-tahina1651286843956.png",
+    "categoryId": 1,
+    "userId": 1,
+    "category": {
+      "id": 1,
+      "name": "Objets"
+    }
+  },
+  {
+    "id": 2,
+    "title": "Appartement Paris V",
+    "imageUrl": "http://localhost:5678/images/appartement-paris-v1651287270508.png",
+    "categoryId": 2,
+    "userId": 1,
+    "category": {
+      "id": 2,
+      "name": "Appartements"
+    }
+  },
+  {
+    "id": 3,
+    "title": "Restaurant Sushisen - Londres",
+    "imageUrl": "http://localhost:5678/images/restaurant-sushisen-londres1651287319271.png",
+    "categoryId": 3,
+    "userId": 1,
+    "category": {
+      "id": 3,
+      "name": "Hotels & restaurants"
+    }
+  },
+  {
+    "id": 4,
+    "title": "Villa “La Balisiere” - Port Louis",
+    "imageUrl": "http://localhost:5678/images/la-balisiere1651287350102.png",
+    "categoryId": 2,
+    "userId": 1,
+    "category": {
+      "id": 2,
+      "name": "Appartements"
+    }
+  },
+  {
+    "id": 5,
+    "title": "Structures Thermopolis",
+    "imageUrl": "http://localhost:5678/images/structures-thermopolis1651287380258.png",
+    "categoryId": 1,
+    "userId": 1,
+    "category": {
+      "id": 1,
+      "name": "Objets"
+    }
+  },
+  {
+    "id": 6,
+    "title": "Appartement Paris X",
+    "imageUrl": "http://localhost:5678/images/appartement-paris-x1651287435459.png",
+    "categoryId": 2,
+    "userId": 1,
+    "category": {
+      "id": 2,
+      "name": "Appartements"
+    }
+  },
+  {
+    "id": 7,
+    "title": "Pavillon “Le coteau” - Cassis",
+    "imageUrl": "http://localhost:5678/images/le-coteau-cassis1651287469876.png",
+    "categoryId": 2,
+    "userId": 1,
+    "category": {
+      "id": 2,
+      "name": "Appartements"
+    }
+  },
+  {
+    "id": 8,
+    "title": "Villa Ferneze - Isola d’Elba",
+    "imageUrl": "http://localhost:5678/images/villa-ferneze1651287511604.png",
+    "categoryId": 2,
+    "userId": 1,
+    "category": {
+      "id": 2,
+      "name": "Appartements"
+    }
+  },
+  {
+    "id": 9,
+    "title": "Appartement Paris XVIII",
+    "imageUrl": "http://localhost:5678/images/appartement-paris-xviii1651287541053.png",
+    "categoryId": 2,
+    "userId": 1,
+    "category": {
+      "id": 2,
+      "name": "Appartements"
+    }
+  },
+  {
+    "id": 10,
+    "title": "Bar “Lullaby” - Paris",
+    "imageUrl": "http://localhost:5678/images/bar-lullaby-paris1651287567130.png",
+    "categoryId": 3,
+    "userId": 1,
+    "category": {
+      "id": 3,
+      "name": "Hotels & restaurants"
+    }
+  },
+  {
+    "id": 11,
+    "title": "Hotel First Arte - New Delhi",
+    "imageUrl": "http://localhost:5678/images/hotel-first-arte-new-delhi1651287605585.png",
+    "categoryId": 3,
+    "userId": 1,
+    "category": {
+      "id": 3,
+      "name": "Hotels & restaurants"
+    }
+  }
+]
+
+// afficherImages(works);
+
